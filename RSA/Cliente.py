507b8d53fb801df4c3a6009984956a0cc98976ec
Caleso <58230946+Caleso@users.noreply.github.com>
2023-11-28 19:41:58 -0300

    Add files via upload

--- RSA/Cliente.py
+++ RSA/Cliente.py
@@ -1,14 +1,9 @@
-#LABORATORIO 6
-#Nombres: Catalina Ledesma y Denisse Torres
-
-#Cliente
-
 import Funciones
 import socket
 
 mensaje_decifrar = []
 
-Host = "LocalHost"
+Host = "localhost"  # Cambiado de "LocalHost" a "localhost"
 Puerto = 8000
 
 Mi_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,30 +12,29 @@ Mi_Socket.connect((Host, Puerto))
 Mensaje_1 = "Recibida variable e\n"
 Mensaje_2 = "Recibida llave n\n"
 Mensaje_3 = "fi de n recibido\n"
-Mensaje_4 = "Mensaje recibido, ahora procedere a descifrar\n"
+Mensaje_4 = "Mensaje recibido, ahora procederé a descifrar\n"
 
-for i in range(1):    
+for i in range(1):
     e = Mi_Socket.recv(1024)
-    e = int(e.decode(encoding = "ascii", errors = "ignore"))
-    
+    e = int(e.decode(encoding="ascii", errors="ignore"))
+
     Mi_Socket.send(Mensaje_1.encode(encoding="ascii", errors="ignore"))
- 
+
     n = Mi_Socket.recv(1024)
-    n = int(n.decode(encoding = "ascii", errors = "ignore"))
+    n = int(n.decode(encoding="ascii", errors="ignore"))
 
     Mi_Socket.send(Mensaje_2.encode(encoding="ascii", errors="ignore"))
-   
+
 for i in range(1):
     fi_n = Mi_Socket.recv(1024)
-    fi_n = int(fi_n.decode(encoding = "ascii", errors = "ignore"))
-    
+    fi_n = int(fi_n.decode(encoding="ascii", errors="ignore"))
+
     Mi_Socket.send(Mensaje_3.encode(encoding="ascii", errors="ignore"))
 
     mensaje_cifrar = Mi_Socket.recv(1024)
-    mensaje_cifrar = Mensaje_Cifrado.decode(encoding = "ascii", errors = "ignore")
-    
+    mensaje_cifrar = mensaje_cifrar.decode(encoding="ascii", errors="ignore")
+
     Mi_Socket.send(Mensaje_4.encode(encoding="ascii", errors="ignore"))
-    
 
 mensaje_cifrar = mensaje_cifrar.split(",")
 mensaje_cifrar.pop()
@@ -48,23 +42,22 @@ mensaje_cifrar.pop()
 for pos in range(0, len(mensaje_cifrar)):
     mensaje_cifrar[pos] = int(mensaje_cifrar[pos])
 
-d = Funciones.modinv(e, fi_n)  #Llave privada
+d = Funciones.modinv(e, fi_n)  # Llave privada
 
-#Descifrado
+# Descifrado
 for m in mensaje_cifrar:
-    descifrado = pow(m, d) % n
-    mensaje_decifrado.append(decifrado)
+    descifrado = pow(int(m), d, n)
+    mensaje_decifrar.append(descifrado)
 
+print("Descifrado en ASCII:", mensaje_decifrar, "\n")
 
-print("Decifrado en ASCII:", mensaje_decifrado, "\n")
+descifrado = ""
 
-decifrado = ""
+for i in mensaje_decifrar:
+    descifrado += chr(i)
 
-for i in mensaje_decifrado:
-    decifrado += chr(i)
-    
-print("Decifrado:", decifrado)
+print("Descifrado:", descifrado)
 
-salida = open('mensajerecibido.txt','w')
-salida.write(decifrado)
+salida = open('mensajerecibido.txt', 'w')
+salida.write(descifrado)
 salida.close()
--- RSA/Servidor.py
+++ RSA/Servidor.py
@@ -1,24 +1,19 @@
-#LABORATORIO 6
-#Nombre: Catalina Ledesma y Denisse Torres
-
-#Servidor
-
 import Funciones
 import socket
 
 letras_ascii = []
 mensaje_cifrar = ""
-mensaje = open('mensajeentrada.txt','r') #Txt que contiene el texto
+mensaje = open('mensajeentrada.txt', 'r')  # Txt que contiene el texto
 
 for i in mensaje.read():
     letras_ascii.append(ord(i))
 mensaje.close()
 
-#Primos privados
+# Primos privados
 P = 569
-Q = 839 
-n = P*Q #Llave publica
-fi_n = (P-1)*(Q-1)
+Q = 839
+n = P * Q  # Llave pública
+fi_n = (P - 1) * (Q - 1)
 
 seguir = True
 while seguir:
@@ -26,19 +21,18 @@ while seguir:
     if Funciones.mcd(e, fi_n) == 1:
         seguir = False
     else:
-        print("El maximo comun divisor debe ser 1")
-        
+        print("El máximo común divisor debe ser 1")
 
-#Cifrado
+# Cifrado
 for m in letras_ascii:
-    cifrado = pow(m, e) % n
-    mensaje_cifrar += str(cifrado)+","
-    
-#VARIABLES PUBLICAS: e, n
-    
-#VARIABLES PRIVADAS: P, Q, fi_n
+    cifrado = pow(m, e, n)
+    mensaje_cifrar += str(cifrado) + ","
 
-Host = "LocalHost"
+# VARIABLES PUBLICAS: e, n
+
+# VARIABLES PRIVADAS: P, Q, fi_n
+
+Host = "localhost"  # Cambiado de "LocalHost" a "localhost"
 Puerto = 8000
 
 Server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -55,17 +49,16 @@ fi_n = str(fi_n)
 for i in range(1):
     Conexion.send(e.encode(encoding="ascii", errors="ignore"))
     Mensaje_1 = Conexion.recv(1024)
-    print((Mensaje_1.decode(encoding = "ascii", errors = "ignore")))
-    
+    print((Mensaje_1.decode(encoding="ascii", errors="ignore")))
+
     Conexion.send(n.encode(encoding="ascii", errors="ignore"))
     Mensaje_2 = Conexion.recv(1024)
-    print((Mensaje_2.decode(encoding = "ascii", errors = "ignore")))
-    
-for i in range(1):
+    print((Mensaje_2.decode(encoding="ascii", errors="ignore")))
+
     Conexion.send(fi_n.encode(encoding="ascii", errors="ignore"))
     Mensaje_3 = Conexion.recv(1024)
-    print((Mensaje_3.decode(encoding = "ascii", errors = "ignore")))
-    
-    Conexion.send(Mensaje_Cifrado.encode(encoding="ascii", errors="ignore"))
+    print((Mensaje_3.decode(encoding="ascii", errors="ignore")))
+
+    Conexion.send(mensaje_cifrar.encode(encoding="ascii", errors="ignore"))
     Mensaje_4 = Conexion.recv(1024)
-    print((Mensaje_4.decode(encoding = "ascii", errors = "ignore")))
+    print((Mensaje_4.decode(encoding="ascii", errors="ignore")))
